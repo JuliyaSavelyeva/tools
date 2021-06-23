56834288f0669c82d6bf4c7192b2d50b99592c27
JuliyaSavelyeva <juliyasavelyeva0605@gmail.com>
2021-06-23 19:10:08 +0300

    lesson6

---
+++ lesson6-loaders/todolist/src/index.js
@@ -0,0 +1,9 @@
+import { initTodoListHandlers } from './list/todoList.js';
+import { renderTasks } from './list/renderer.js';
+import { getTasksList } from './list/tasksGateway.js';
+import './index.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  getTasksList().then(() => renderTasks());
+  initTodoListHandlers();
+});
---
+++ lesson6-loaders/todolist/src/list/renderer.js
@@ -0,0 +1,44 @@
+import { getTasksList } from './tasksGateway.js';
+
+const listElem = document.querySelector('.list');
+
+const createCheckbox = ({ done, id }) => {
+  const checkboxElem = document.createElement('input');
+  checkboxElem.setAttribute('type', 'checkbox');
+  checkboxElem.setAttribute('data-id', id);
+  checkboxElem.checked = done;
+  checkboxElem.classList.add('list-item__checkbox');
+
+  return checkboxElem;
+};
+
+const createListItem = ({ text, done, id }) => {
+  const listItemElem = document.createElement('li');
+  listItemElem.classList.add('list-item');
+  const checkboxElem = createCheckbox({ done, id });
+  if (done) {
+    listItemElem.classList.add('list-item_done');
+  }
+
+  const textElem = document.createElement('span');
+  textElem.classList.add('list-item__text');
+  textElem.textContent = text;
+
+  const deleteBtnElem = document.createElement('button');
+  deleteBtnElem.classList.add('list-item__delete-btn');
+  deleteBtnElem.setAttribute('data-id', id);
+
+  listItemElem.append(checkboxElem, textElem, deleteBtnElem);
+  return listItemElem;
+};
+
+export const renderTasks = () => {
+  const render = tasksList => {
+    listElem.innerHTML = '';
+    const tasksElem = tasksList.sort((a, b) => a.done - b.done).map(createListItem);
+
+    listElem.append(...tasksElem);
+  };
+
+  getTasksList().then(tasksList => render(tasksList));
+};
